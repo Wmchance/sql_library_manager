6de6fe985ef93d73df1af60c868632936c1719d2
Will <wmchance@gmail.com>
2022-10-10 19:35:54 +0700

    Add basic 'books' route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const booksRouter = require('./routes/books');
 
 const app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/books', booksRouter)
 
 // Add static middleware
 app.use('/static', express.static('public'));
---
+++ routes/books.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
+/* GET books page. */
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Book.findAll(); //get all the books, and store them in a variable
+  res.render('books', { books });
+  //res.json(books); //display the books on a webpage
+}));
+
+module.exports = router;
