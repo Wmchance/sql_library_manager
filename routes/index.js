37f73c8ef198ccb8377f5e1ee67d31a5989de524
Will <wmchance@gmail.com>
2022-10-10 15:18:57 +0700

    Test Book model & communication with the database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res) => {
+  //res.render('index', { title: 'Express' });
+  const books = await Book.findAll(); //get all the books, and store them in a variable
+  res.json(books); //display the books on a webpage
+}));
 
 module.exports = router;
