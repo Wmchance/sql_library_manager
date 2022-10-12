9e3658edf27278a778fac0f0fd2562864af84956
Will <wmchance@gmail.com>
2022-10-12 10:28:57 +0700

    Add page-not-found view

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/static', express.static('public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404, 'Sorry, but it looks like that location does not exsit'));
+  next(createError(404, 'Sorry, but it looks like that page does not exsit.'));
 });
 
 // error handler
@@ -54,10 +54,15 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // console.log(err.status);
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  if(err.status === 404) {
+    res.render('page-not-found');
+  } else {
+    res.render('error');
+  }
 });
 
 module.exports = app;
